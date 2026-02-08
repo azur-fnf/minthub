2e7e5aaaa1e3136322f2cd6d0b7384bbcc4b40f0
Azur <durkmcteam@gmail.com>
2026-02-08 15:37:30 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,21 @@
 local placeId = game.PlaceId
 
-if placeId == 7205641391 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/azur-fnf/minthub/refs/heads/main/mondaymorningmisery/script.lua"))()
+local places = {
+    [7205641391] = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/azur-fnf/minthub/refs/heads/main/mondaymorningmisery/script.lua"))()
+    end,
+
+    [1234567890] = function()
+        print("oi estou em outro mapa")
+    end
+}
+
+if places[placeId] then
+    places[placeId]()
+else
+    StarterGui:SetCore("SendNotification", {
+        Title = "Jogo não suportado",
+        Text = "O jogo '" .. placeName .. "' não é suportado por este script.",
+        Duration = 6
+    })
 end
