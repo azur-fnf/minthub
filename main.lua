e4ee0da44c6743770346eb071fca2603078aaf32
Azur <durkmcteam@gmail.com>
2026-02-08 15:34:02 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+local placeId = game.PlaceId
+
+if placeId == 7205641391 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/azur-fnf/minthub/refs/heads/main/mondaymorningmisery/script.lua"))()
+end
